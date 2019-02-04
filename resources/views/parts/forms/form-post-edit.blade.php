c221c663b35394c4f5c8f8e7b435bad4e47a188e
displayflex <3tonishere@gmail.com>
2019-02-04 12:03:21 +0300

    Fixes display of old content in update forms

--- resources/views/parts/forms/form-post-edit.blade.php
+++ resources/views/parts/forms/form-post-edit.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="form-item {{ $errors->has('title') ? 'has-error' : '' }}">
 		<label for="post-form__title">Заголовок <span>*</span></label>
-		<input name="title" type="text" placeholder="Введите заголовок поста" id="post-form__title" value="{{ $post->title }}">
+		<input name="title" type="text" placeholder="Введите заголовок поста" id="post-form__title" value="{{ old('title', $post->title) }}">
 	</div>
 
 	<div class="form-item {{ $errors->has('section') ? 'has-error' : '' }}">
@@ -25,7 +25,7 @@
 
 	<div class="form-item {{ $errors->has('content') ? 'has-error' : '' }}">
 		<label for="post-form__content">Контент <span>*</span></label>
-		<textarea name="content" id="post-form__content" placeholder="Введите содержимое поста">{{ $post->content }}</textarea>
+		<textarea name="content" id="post-form__content" placeholder="Введите содержимое поста">{{ old('content', $post->content) }}</textarea>
 	</div>
 
 	<div class="form-item">
--- resources/views/parts/forms/form-user-edit.blade.php
+++ resources/views/parts/forms/form-user-edit.blade.php
@@ -3,32 +3,68 @@
 
 	<div class="form-item">
 		<label for="profile__login">Логин</label>
-		<input name="login" type="text" placeholder="Введите логин" id="profile__login" value="{{ $user->login }}" disabled>
+		<input
+			name="login"
+			type="text"
+			placeholder="Введите логин"
+			id="profile__login"
+			value="{{ $user->login }}"
+			disabled
+		>
 	</div>
 
 	<div class="form-item {{ $errors->has('email') ? 'has-error' : '' }}">
 		<label for="profile__email">E-mail</label>
-		<input name="email" type="text" placeholder="Введите e-mail" id="profile__email" value="{{ $user->email }}">
+		<input
+			name="email"
+			type="text"
+			placeholder="Введите e-mail"
+			id="profile__email"
+			value="{{ old('email', $user->email) }}"
+		>
 	</div>
 
 	<div class="form-item {{ $errors->has('name') ? 'has-error' : '' }}">
 		<label for="profile__name">Имя</label>
-		<input name="name" type="text" placeholder="Введите имя" id="profile__name" value="{{ $user->profile->name ?? '' }}">
+		<input
+			name="name"
+			type="text"
+			placeholder="Введите имя"
+			id="profile__name"
+			value="{{ old('name', $user->profile->name ?? '') }}"
+		>
 	</div>
 
 	<div class="form-item {{ $errors->has('surname') ? 'has-error' : '' }}">
 		<label for="profile__surname">Фамилия</label>
-		<input name="surname" type="text" placeholder="Введите фамилию" id="profile__surname" value="{{ $user->profile->surname ?? '' }}">
+		<input
+			name="surname"
+			type="text"
+			placeholder="Введите фамилию"
+			id="profile__surname"
+			value="{{ old('surname', $user->profile->surname ?? '') }}"
+		>
 	</div>
 
 	<div class="form-item {{ $errors->has('phone') ? 'has-error' : '' }}">
 		<label for="profile__phone">Телефон</label>
-		<input name="phone" type="text" placeholder="Введите номер телефона" id="profile__phone" value="{{ $user->profile->phone ?? '' }}">
+		<input
+			name="phone"
+			type="text"
+			placeholder="Введите номер телефона"
+			id="profile__phone" value="{{ old('phone', $user->profile->phone ?? '') }}"
+		>
 	</div>
 
 	<div class="form-item {{ $errors->has('birthdate') ? 'has-error' : '' }}">
 		<label for="profile__birthdate">Дата рождения</label>
-		<input name="birthdate" type="date" placeholder="Введите Ваш день рождения" id="profile__birthdate" value="{{ $user->profile->birthdate ?? '' }}">
+		<input
+			name="birthdate"
+			type="date"
+			placeholder="Введите Ваш день рождения"
+			id="profile__birthdate"
+			value="{{ old('birthdate', $user->profile->birthdate ?? '') }}"
+		>
 	</div>
 
 	<div class="form-item">
